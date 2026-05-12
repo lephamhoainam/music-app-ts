4180429ec5656ca39132bdde85ed61f57e967735
muahahammm <aahh853853@gmail.com>
2026-05-12 14:49:10 +0700

    Nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -8,7 +8,9 @@ database.connect();
 import clientRoute from "./routes/client/index.route";
 
 const app: Express = express();
-const port: Number = 3000;
+const port: Number | String = process.env.PORT || 3000;
+
+app.use(express.static("public")); 
 
 app.set("views", "./views");
 app.set("view engine", "pug");
