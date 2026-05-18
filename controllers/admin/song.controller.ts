0502e948a92c7a930de71960ce592c0daa31d3ed
muahahammm <aahh853853@gmail.com>
2026-05-18 21:50:14 +0700

    Làm trang quản lý bài hát

---
+++ controllers/admin/song.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+
+import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
+
+// [GET] /admin/songs
+export const index = async (req: Request, res: Response) => {
+    const songs = await Song.find({
+        deleted: false
+    });
+
+    for (const song of songs) {
+        const infoTopic = await Topic.findOne({
+            _id: song.topicId
+        });
+
+        (song as any).infoTopic = infoTopic;
+    }
+
+    for (const song of songs) {
+        const infoSinger = await Singer.findOne({
+            _id: song.singerId
+        });
+
+        (song as any).infoSinger = infoSinger;
+    }
+
+    res.render("admin/pages/songs/index", {
+        pageTitle: "Quản lý bài hát",
+        songs: songs
+    });
+}
--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -3,6 +3,7 @@ import { systemConfig } from "../../config/config";
 
 import { dashboardRoute } from "./dashboard.route";
 import { topicRoute } from "./topic.route";
+import { songRoute } from "./song.route";
 
 const adminRoute = (app: Express): void => {
     const pathAdmin = systemConfig.prefixAdmin;
@@ -10,6 +11,8 @@ const adminRoute = (app: Express): void => {
     app.use(`/${pathAdmin}/dashboard`, dashboardRoute);
 
     app.use(`/${pathAdmin}/topics`, topicRoute);
+
+    app.use(`/${pathAdmin}/songs`, songRoute);
 }
 
 export default adminRoute;
