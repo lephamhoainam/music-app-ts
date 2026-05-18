c4a8d08f3a765f0c1db3b0bf3c731e90de5c9943
muahahammm <aahh853853@gmail.com>
2026-05-18 21:27:43 +0700

    Làm trang chủ đề bài hát

--- controllers/admin/dashboard.controller.ts
+++ controllers/admin/dashboard.controller.ts
@@ -4,6 +4,6 @@ import { Request, Response } from "express";
 export const index = async (req: Request, res: Response) => {
     
     res.render("admin/pages/dashboard/index", {
-        pageTitle: "Bài hát yêu thích"
+        pageTitle: "Trang tổng quan - ADMIN"
     });
 }
--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -2,11 +2,14 @@ import { Express } from "express";
 import { systemConfig } from "../../config/config";
 
 import { dashboardRoute } from "./dashboard.route";
+import { topicRoute } from "./topic.route";
 
 const adminRoute = (app: Express): void => {
     const pathAdmin = systemConfig.prefixAdmin;
 
     app.use(`/${pathAdmin}/dashboard`, dashboardRoute);
+
+    app.use(`/${pathAdmin}/topics`, topicRoute);
 }
 
 export default adminRoute;
