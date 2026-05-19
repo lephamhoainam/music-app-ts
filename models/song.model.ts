288c199f96bc8899637bd2570db410a4180c4050
muahahammm <aahh853853@gmail.com>
2026-05-19 18:18:47 +0700

    update model song

--- models/song.model.ts
+++ models/song.model.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+const slug = require("mongoose-slug-updater");
+
+mongoose.plugin(slug);
 
 const songSchema = new mongoose.Schema({
     title: String,
@@ -17,7 +20,11 @@ const songSchema = new mongoose.Schema({
     lyrics: String, 
     audio: String, 
     status: String,
-    slug: String,
+    slug: {
+        type: String,
+        slug: "title",
+        unique: true
+    },
     deleted: { 
         type: Boolean, 
         default: false 
