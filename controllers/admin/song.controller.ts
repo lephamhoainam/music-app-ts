bc647b007f783dd226ccaf25cf0e2adba87c0ac6
muahahammm <aahh853853@gmail.com>
2026-05-21 21:08:31 +0700

    Nâng cấp

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -56,7 +56,18 @@ export const create = async (req: Request, res: Response) => {
 
 // [POST] /admin/songs/create
 export const createPost = async (req: Request, res: Response) => {
-    const song = new Song(req.body);
+    const dataSong = {
+        title: req.body.title, 
+        topicId: req.body.topicId,
+        singerId: req.body.singerId,
+        description: req.body.description,
+        status: req.body.status,
+        avatar: req.body.avatar,
+        lyrics: req.body.lyrics,
+        // audio: req.body.audio
+    }
+
+    const song = new Song(dataSong);
     await song.save();
 
     res.redirect(`/${systemConfig.prefixAdmin}/songs`);
