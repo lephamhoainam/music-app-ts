06fcb138645209595f0f064ddebc13e65648007b
muahahammm <aahh853853@gmail.com>
2026-05-21 23:33:00 +0700

    Làm chức năng thêm chủ đề

--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
+import { systemConfig } from "../../config/config";
 
 // [GET] /admin/topics
 export const index = async (req: Request, res: Response) => {
@@ -11,4 +12,33 @@ export const index = async (req: Request, res: Response) => {
         pageTitle: "Quản lý chủ đề bài hát",
         topics: topics
     });
+}
+
+
+// [GET] /admin/topics/create
+export const create = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false 
+    });
+
+    res.render("admin/pages/topics/create", {
+        pageTitle: "Thêm mới chủ đề bài hát",
+        topics: topics
+    });
+}
+
+
+// [POST] /admin/topics/create
+export const createPost = async (req: Request, res: Response) => {
+    const dataTopic = {
+        title: req.body.title,
+        status: req.body.status,
+        description: req.body.description,
+        avatar: req.body.avatar
+    }
+
+    const topic = new Topic(dataTopic);
+    await topic.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/topics`);
 }
--- models/topic.model.ts
+++ models/topic.model.ts
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
+const slug = require("mongoose-slug-updater");
+
+mongoose.plugin(slug);
 
 const topicSchema = new mongoose.Schema({
     title: String,
     avatar: String,
     description: String,
     status: String,
-    slug: String,
+    slug: {
+        type: String,
+        slug: "title",
+        unique: true
+    },
     deleted: { 
         type: Boolean, 
         default: false 
