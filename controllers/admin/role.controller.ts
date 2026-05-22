fa644d65c703e934bd9a888dbce3855f69002e0d
muahahammm <aahh853853@gmail.com>
2026-05-22 08:16:34 +0700

    Tạo trang nhóm quyền

--- controllers/admin/role.controller.ts
+++ controllers/admin/role.controller.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 
+import Role from "../../models/role.model";
 import { systemConfig } from "../../config/config";
 
 // [GET] /admin/roles
 export const index = async (req: Request, res: Response) => {
+    const roles = await Role.find({
+        deleted: false
+    });
+
     res.render("admin/pages/roles/index", {
-        pageTitle: "Quản lý ca sĩ"
+        pageTitle: "Phân quyền hệ thống",
+        roles: roles
     });
 }
