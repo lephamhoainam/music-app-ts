bb0383dd60d1949b94490335a54f6f90512667ce
muahahammm <aahh853853@gmail.com>
2026-05-23 15:49:07 +0700

    Làm chức năng thêm mới tài khoản admin

--- controllers/admin/account.controller.ts
+++ controllers/admin/account.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 
+import md5 from 'md5';
+
 import Account from "../../models/account.model";
 import Role from "../../models/role.model";
+import { systemConfig } from "../../config/config";
 
 // [GET] /admin/accounts
 export const index = async (req: Request, res: Response) => {
@@ -26,4 +29,20 @@ export const create = async (req: Request, res: Response) => {
         pageTitle: "Tạo mới tài khoản hệ thống",
         roles: roles
     });
+}
+
+
+// [POST] /admin/accounts/create
+export const createPost = async (req: Request, res: Response) => {
+    const dataAccount = {
+        email: req.body.email, 
+        password: md5(req.body.password),
+        roleId: req.body.roleId,
+        status: req.body.status
+    }
+
+    const newAccount = new Account(dataAccount);
+    await newAccount.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 }
--- routes/admin/account.route.ts
+++ routes/admin/account.route.ts
@@ -7,4 +7,6 @@ router.get("/", controller.index);
 
 router.get("/create", controller.create);
 
+router.post("/create", controller.createPost);
+
 export const accountRoute: Router = router;
