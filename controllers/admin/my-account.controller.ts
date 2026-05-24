d35ede86e2ad4def0ff3c3ca336a1f6014506f9d
muahahammm <aahh853853@gmail.com>
2026-05-24 13:02:11 +0700

    Làm chức năng chỉnh sửa tài khoản của bản thân

--- controllers/admin/my-account.controller.ts
+++ controllers/admin/my-account.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import Role from "../../models/role.model";
 
 import { systemConfig } from "../../config/config";
+import Account from "../../models/account.model";
 
 // [GET] /admin/my-account
 export const index = async (req: Request, res: Response) => {
@@ -16,4 +17,34 @@ export const index = async (req: Request, res: Response) => {
         pageTitle: "Tài khoản của tôi",
         roles: roles
     });
+}
+
+
+// [POST] /admin/my-account
+export const editPost = async (req: Request, res: Response) => {
+    const userId = res.locals.user;
+    const updatedData: any= {};
+
+    // Loại bỏ field rỗng
+    Object.keys(req.body).forEach((key) => {
+        if(req.body[key] === '')
+            delete req.body[key];
+    });
+    // Kết thúc loại bỏ field rỗng
+
+    // Thêm filed đã thay đổi vào object
+    for (const key in req.body) {
+        if(String(req.body[key]) !== String(userId[key])) {
+            updatedData[key] = req.body[key];
+        }
+    }
+    // Kết thúc thêm filed đã thay đổi vào object
+
+    if(Object.keys(updatedData).length > 0) {
+        await Account.updateOne({
+            _id: userId._id
+        }, updatedData);
+    }
+
+    res.redirect(`/${systemConfig.prefixAdmin}/my-account`);
 }
--- routes/admin/my-account.route.ts
+++ routes/admin/my-account.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 const router: Router = Router();
 
 import * as controller from "../../controllers/admin/my-account.controller";
+import * as uploadCloud from "../../middlewares/admin/uploadCloud.middleware";
+
+const upload = multer();
 
 router.get("/", controller.index);
+
+router.post("/", upload.single("avatar"), uploadCloud.uploadSingle, controller.editPost);
 
 export const myAccountRoute: Router = router;
